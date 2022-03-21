cab710d235aef3a4ac5d8e572af07129f7d16fc8
MarekMichal <73107526+MarekMichal@users.noreply.github.com>
2022-03-21 21:06:38 +0100

    Add files via upload

---
+++ php-main/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lnag="en">
+    <style>
+        body {
+            text-align: center;
+            background-color: lightgray;
+        }      
+        a {
+            color: black;
+            font-size: 50px;
+            border: 3px black solid;
+            background-color: khaki;
+            text-decoration: none;
+            font-weight: bold;
+        } 
+    </style>
+<head>
+    <meta charshet="UTF-8">
+    <title>Hlavna stranka</title>
+<body>
+    <a href="register.php">Registracia</a>
+    <p><a href="login.php">Prihlasenie</a>
+</body>
+</html>
